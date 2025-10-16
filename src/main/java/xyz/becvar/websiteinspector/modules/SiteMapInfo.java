0329e577afa9974c59d5a7ae1da06e5a3cfbd812
lukasbecvar <becvarlukas99@gmail.com>
2025-10-16 20:18:36 +0200

    fixed display of sitemap results if sitemap or robots.txt is not found

--- src/main/java/xyz/becvar/websiteinspector/modules/SiteMapInfo.java
+++ src/main/java/xyz/becvar/websiteinspector/modules/SiteMapInfo.java
@@ -26,37 +26,42 @@ public class SiteMapInfo implements AnalysisModule {
     public AnalysisResult analyze(String targetUrl) {
         String robotsSummary = getRobotsTxtAnalyze(targetUrl);
         String sitemapSummary = getSitemapAnalyze(targetUrl);
+
+        if (robotsSummary == null && sitemapSummary == null) {
+            return null;
+        }
+
         return new SiteMapInfoResult(robotsSummary, sitemapSummary);
     }
 
     /**
      * Analyzes the robots.txt file for the given URL
-     * 
+     *
      * @param url The URL to analyze
-     * 
+     *
      * @return A string containing the summary of the robots.txt file
      */
     private String getRobotsTxtAnalyze(String url) {
         String robotsUrl = url.endsWith("/") ? url + "robots.txt" : url + "/robots.txt";
         String content = WebsiteUtils.downloadFileContent(robotsUrl);
         if (content == null || content.isEmpty()) {
-            return "robots.txt file not found or is empty.";
+            return null;
         }
         return content;
     }
 
     /**
      * Analyzes the sitemap.xml file for the given URL
-     * 
+     *
      * @param url The URL to analyze
-     * 
+     *
      * @return A string containing the summary of the sitemap.xml file
      */
     private String getSitemapAnalyze(String url) {
         String sitemapUrl = url.endsWith("/") ? url + "sitemap.xml" : url + "/sitemap.xml";
         String content = WebsiteUtils.downloadFileContent(sitemapUrl);
         if (content == null || content.isEmpty()) {
-            return "sitemap.xml file not found or is empty.";
+            return null;
         }
         return content;
     }
@@ -75,17 +80,21 @@ public class SiteMapInfo implements AnalysisModule {
 
         @Override
         public void print() {
-            // robots.txt summary
-            Logger.printSpacer();
-            Logger.log("Robots.txt Summary");
-            Logger.printSpacer();
-            Logger.rawLog(robotsSummary);
+            if (robotsSummary != null) {
+                // robots.txt summary
+                Logger.printSpacer();
+                Logger.log("Robots.txt Summary");
+                Logger.printSpacer();
+                Logger.rawLog(robotsSummary);
+            }
 
-            // sitemap.xml summary
-            Logger.printSpacer();
-            Logger.log("Sitemap.xml Summary");
-            Logger.printSpacer();
-            Logger.rawLog(sitemapSummary);
+            if (sitemapSummary != null) {
+                // sitemap.xml summary
+                Logger.printSpacer();
+                Logger.log("Sitemap.xml Summary");
+                Logger.printSpacer();
+                Logger.rawLog(sitemapSummary);
+            }
         }
     }
 }
